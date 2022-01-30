6dbc2ebf200d1ccd01147530f66921a248c90ccf
Abhishek-90 <holaniabhishek90@gmail.com>
2022-01-30 16:33:59 +0530

    Added Config File with URLs

---
+++ src/app/Config/constant.ts
@@ -0,0 +1,4 @@
+export const API_ENDPOINT_Everything = 'https://newsapi.org/v2/everything?q='
+export const Endpoint_Top_Headlines = 'https://newsapi.org/v2/top-headlines'
+export const General_Category_News = 'https://newsapi.org/v2/top-headlines?category=general'
+export const API_KEY = '4c23e36c9f07473097374db50f3f5a9c'
